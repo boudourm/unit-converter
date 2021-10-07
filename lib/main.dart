9bc45c52bc25383008d2a7aee349c24415921e2b
boudourm <mehdiboudour.contact@gmail.com>
2021-10-08 01:07:49 +0200

    15. Code the Unit Converter AppBar and Category Route

--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: unused_element
-
 // You can read about packages here: https://flutter.dev/using-packages/
 import 'package:flutter/material.dart';
+import 'package:unit_converter/category_route.dart';
 
-// You can use a relative import, i.e. `import 'category.dart';` or
-// a package import, as shown below.
-// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'package:unit_converter/category.dart';
-
-  // TODO: Pass this information into your custom [Category] widget
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
+// TODO: Import the CategoryRoute widget
 
 /// The function that is called when main.dart is run.
- void main() {
+void main() {
   runApp(const UnitConverterApp());
 }
 
 /// This widget is the root of our application.
-/// Currently, we just show one widget in our app.
+///
+/// The first screen we see is a list [Categories].
 class UnitConverterApp extends StatelessWidget {
   const UnitConverterApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: const Center(
-          // TODO: Determine what properties you'll need to pass into the widget
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      // TODO: Instead of pointing to exactly 1 Category widget,
+      // our home should now point to an instance of the CategoryRoute widget.
+      home: CategoryRoute(),
     );
   }
 }
